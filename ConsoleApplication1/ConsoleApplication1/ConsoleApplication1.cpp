af6afad93a089747f05b0d609713677b09913df9
于珂珂 yukk@hiscene.com <1416560660@qq.com>
2017-06-14 11:13:55 +0800

    no message

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,8 +1,9 @@
-
-
-#include "stdafx.h"
-#include<iostream>
-void main(){
-    std::cout << "xzsdsfdsfdf" << std::endl;
-   
-}
+//#include"stdafx.h"
+//#include<iostream>
+//void main(){
+//
+//    std::cout << "cdvxvd" << std::endl;
+//
+//    std::cout << "xzsdsfdsfdf" << std::endl;
+//   
+//}
---
+++ testDemo/testDemo/demo.cpp
@@ -0,0 +1,18 @@
+#include "demo.h"
+#include<cstdlib>
+
+demo::demo()
+{
+}
+
+
+demo::~demo()
+{
+}
+
+
+void main(){
+    std::cout << "sdyyyyyyysfsfsf" << std::endl;
+
+    system("pause");
+}
